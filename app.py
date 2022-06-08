bca8f2bc87854624819e882f562e99309390afdf
MarcTowse <marctowse@hotmail.com>
2022-06-08 22:01:25 +0800

    flutter added

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask
+from flask import send_from_directory
+from flask import render_template
+
 app = Flask(__name__)
 
+FLUTTER_WEB_APP = 'templates'
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+@app.route('/web/')
+def render_page_web():
+    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route('/web/<path:name>')
+def return_flutter_doc(name):
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    datalist = str(name).split('/')
+    DIR_NAME = FLUTTER_WEB_APP
+
+    if len(datalist) > 1:
+        for i in range(0, len(datalist) - 1):
+            DIR_NAME += '/' + datalist[i]
+
+    return send_from_directory(DIR_NAME, datalist[-1])
 
 
 if __name__ == '__main__':
